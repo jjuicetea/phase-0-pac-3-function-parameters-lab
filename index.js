8e90f25885aec5dc65ba97b29f53bc63287b02cf
James Giusti <jamez318@gmail.com>
2021-08-21 13:05:39 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function introduction(name) {
+    console.log(`Hi, my name is ${Josh}.`);
+
+}
+
+introduction(James);
+
+function introductionWithLanguage(name, language) {
+    console.log(`Hi, my name is ${name} and I am learning to code in ${language}.`);
+
+}
+
+introductionWithLanguage(James, Javascript);
